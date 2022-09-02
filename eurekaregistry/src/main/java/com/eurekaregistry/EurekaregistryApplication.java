46ad46473f1df6cbf71e32971a9446735319dea6
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2022-09-02 12:48:44 +0530

    bxcvx

---
+++ eurekaregistry/src/main/java/com/eurekaregistry/EurekaregistryApplication.java
@@ -0,0 +1,17 @@
+package com.eurekaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaregistryApplication.class, args);
+	}
+
+}
